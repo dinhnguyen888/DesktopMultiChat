fc95b786e2093afde38c3a921e62274d347a9d7b
dinhnguyen888 <nguyenphucdinh89@gmail.com>
2025-03-15 01:17:15 +0700

    chore: add Dashboard, videoCall view sample and remove some unuse code

--- DesktopChat/Views/SettingView.xaml.cs
+++ DesktopChat/Views/SettingView.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DesktopChat.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +24,7 @@ namespace DesktopChat.Views
         public SettingView()
         {
             InitializeComponent();
+            DataContext = new SettingVM();
         }
     }
 }
